579c0d022d67f2f28b3eabaac1bd745baac8b605
root <root@ubuntu.Stickfigure.com>
2014-11-20 13:37:50 -0500

    initial node app set up

---
+++ app.js
@@ -0,0 +1,28 @@
+ora_js = {};
+
+ora_js.AWS = require('aws-sdk');
+ora_js.AWS.config.update({accessKeyId: 'AKIAIXWFTTMHQKL6TPUA', secretAccessKey: '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'});
+ora_js.AWS.config.update({region: 'us-east-1'});
+
+ora_js.dynamodb = new ora_js.AWS.DynamoDB();
+ora_js.s3 = new ora_js.AWS.S3();
+
+ora_js.usw = new ora_js.AWS.S3({region: 'us-east-1'});
+
+pg = require("pg");
+ora_js.cn = "pg://postgres:sheba1810@th.ora.tv:5432/oratv_dev";
+
+var express = require('express');
+app = express();
+//app.use(express.bodyParser());
+//app.use(express.favicon(__dirname + '/public/favicon.ico'));
+
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+var server = app.listen(1810);
+console.log('Listening on port 1810 ');
+
+app.use(express.static(__dirname + '/public'));
+
+var util	= require('./routes/util.js');
